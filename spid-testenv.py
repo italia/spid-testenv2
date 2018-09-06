22754949b2881af8fb6f905f3e16c340f9299151
Roberto Polli <robipolli@gmail.com>
2018-09-06 11:02:04 +0200

    Fix: #153. Create a self-signed certificate via the --dummy-cert parameter.
      Using --dummy-cert parameter you can run the test environment
      without running external commands.

--- spid-testenv.py
+++ spid-testenv.py
@@ -7,9 +7,11 @@ import os.path
 
 from flask import Flask
 
+from saml2.cert import OpenSSLWrapper
 from testenv.exceptions import BadConfiguration
 from testenv.server import IdpServer
 from testenv.utils import get_config
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -21,9 +23,35 @@ if __name__ == '__main__':
         '-ct', dest='configuration_type',
         help='Configuration type [yaml|json]', default='yaml'
     )
+    parser.add_argument(
+        '--dummy-cert', dest='dummy_cert', action="store_true",
+        help='Generate and use dummy self-signed certificate in .tmpdir/ and ignore the settings in config.yaml', default=False
+    )
+
     args = parser.parse_args()
     # Init server
     config = get_config(args.config, args.configuration_type)
+
+    if args.dummy_cert:
+        if not os.path.exists('.tmpdir'):
+            os.mkdir('.tmpdir')
+        openssl = OpenSSLWrapper()
+        config['cert_file'], config['key_file'] = openssl.create_certificate(
+            {
+                "cn": "localhost",
+                "country_code": "IT",
+                "state": "Lazio",
+                "city": "Roma",
+                "organization":
+                "MyCompany",
+                "organization_unit": "IDP"
+            },
+            request=False,
+            write_to_file=True,
+            key_length=2048,
+            cert_dir=".tmpdir"
+        )
+
     try:
         os.environ['FLASK_ENV'] = 'development'
         server = IdpServer(
