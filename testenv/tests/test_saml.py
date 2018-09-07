48ba5141d7a913e6cb8901a0ae7e892438ac81a9
Fabio Sangiovanni <f.sangiovanni@nephila.it>
2018-09-07 10:22:56 +0200

    add test to ensure Issuer is both in Response and in Assertion (https://github.com/italia/spid-testenv2/issues/145)

--- testenv/tests/test_saml.py
+++ testenv/tests/test_saml.py
@@ -6,7 +6,7 @@ import unittest
 from lxml import etree
 
 from testenv.saml import create_response
-from testenv.settings import SAML, SPID_LEVEL_1, STATUS_SUCCESS
+from testenv.settings import SAML, SAMLP, SPID_LEVEL_1, STATUS_SUCCESS
 
 
 class SamlElementTestCase(unittest.TestCase):
@@ -52,3 +52,50 @@ class SamlElementTestCase(unittest.TestCase):
         )
         authenticating_authorities = response._element.findall('.//{%s}AuthenticatingAuthority' % SAML)
         self.assertEqual(len(authenticating_authorities), 0)
+
+    def test_issuer_in_response_and_assertion(self):
+        # https://github.com/italia/spid-testenv2/issues/145
+        response = create_response(
+            {
+                'response': {
+                    'attrs': {
+                        'in_response_to': 'test_12345',
+                        'destination': 'http://some.dest.nation'
+                    }
+                },
+                'issuer': {
+                    'attrs': {
+                        'name_qualifier': 'http://test_id.entity',
+                    },
+                    'text': 'http://test_id.entity'
+                },
+                'name_id': {
+                    'attrs': {
+                        'name_qualifier': 'http://test_id.entity',
+                    }
+                },
+
+                'subject_confirmation_data': {
+                    'attrs': {
+                        'recipient': 'http://test_id.entity',
+                    }
+                },
+                'audience': {
+                    'text': 'http://test_sp_id.entity',
+                },
+                'authn_context_class_ref': {
+                    'text': SPID_LEVEL_1
+                }
+            },
+            {
+                'status_code': STATUS_SUCCESS
+            },
+            {}
+        )
+        issuers = response._element.findall('.//{%s}Issuer' % SAML)
+        self.assertEqual(len(issuers), 2)
+        for issuer in issuers:
+            self.assertEqual(issuer.get('NameQualifier'), 'http://test_id.entity')
+            self.assertEqual(issuer.text, 'http://test_id.entity')
+        self.assertEqual(issuers[0].getparent().tag, '{%s}Response' % SAMLP)
+        self.assertEqual(issuers[1].getparent().tag, '{%s}Assertion' % SAML)
